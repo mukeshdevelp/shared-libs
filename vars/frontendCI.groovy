781d431a40996dd844f737ad3da4efde6cae7a87
mukesh <mukesh@gmail.com>
2026-04-01 18:48:52 +0530

    changing the archive folder

--- vars/frontendCI.groovy
+++ vars/frontendCI.groovy
@@ -42,7 +42,7 @@ def call(Map config = [:]) {
                 }
                 post {
                     success {
-                        archiveArtifacts artifacts: 'dist/**', fingerprint: true
+                        archiveArtifacts artifacts: 'build/**', fingerprint: true
                     }
                 }
             }
