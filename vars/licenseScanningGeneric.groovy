875aaec257ef2a1f4e3ed85a3dced7cba82d8780
mukesh <mukesh@gmail.com>
2026-04-07 12:23:45 +0530

    fixed for satge error

--- vars/licenseScanningGeneric.groovy
+++ vars/licenseScanningGeneric.groovy
@@ -37,7 +37,8 @@ def call(Map config = [:]) {
         }
 
         stage('License Scan') {
-            sh """
+            step{
+                sh """
                 mkdir -p ${REPORT_DIR}
                 pwd
                 ls -al
@@ -47,15 +48,17 @@ def call(Map config = [:]) {
                 --format json \
                 --output ${REPORT_DIR}/trivy-license-report.json
             """
-            script {
-                def report = readJSON file: "${REPORT_DIR}/trivy-license-report.json"
-                def totalLicenses = report.Results.collect { it?.Licenses?.size() ?: 0 }.sum()
-                def THRESHOLD = 10
+                script {
+                    def report = readJSON file: "${REPORT_DIR}/trivy-license-report.json"
+                    def totalLicenses = report.Results.collect { it?.Licenses?.size() ?: 0 }.sum()
+                    def THRESHOLD = 10
 
-                if (totalLicenses > THRESHOLD) {
-                    error("License scan failed: Found ${totalLicenses} licenses, exceeds threshold of ${THRESHOLD}.")
+                    if (totalLicenses > THRESHOLD) {
+                        error("License scan failed: Found ${totalLicenses} licenses, exceeds threshold of ${THRESHOLD}.")
+                    }
                 }
             }
+            
         }
 
         echo "License scan completed successfully."
